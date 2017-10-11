d1ddff5069f661c0bc5ebfbfc14a9fe0354f9822
Kayla Hansen <shiverr@gmail.com>
2017-10-11 02:24:41 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,12 @@ def batch_badge_creator(attendees)
     badge_maker(name)
 end
 end
+
+def assign_rooms(speakers)
+  new_array = []
+  speakers.each_with_index do |attendee, index|
+  new_array << attendee + index
+  return "Hello, #{attendee}! You'll be assigned to room #{index}!"
+end
+  new_array
+end    
