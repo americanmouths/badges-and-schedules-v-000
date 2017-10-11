f5217f94c9d3fb5cc8d564e6d3de7a02aa324c29
Kayla Hansen <shiverr@gmail.com>
2017-10-11 02:03:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,11 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  new_array = []
+  attendees.each do |attendee|
+    new_array.push.attendee
+    return new_array
+end
+end
