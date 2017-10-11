8d9e4f4e296e0b402be7a15037145339e59fc651
Kayla Hansen <shiverr@gmail.com>
2017-10-11 03:05:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,7 +24,7 @@ def printer(attendees)
 batch_badge_creator(attendees).each do |name|
   puts name
 end
-assign_rooms(speakers).each do |speaker|
+assign_rooms(attendees).each do |speaker|
   puts speaker
 end
 end
